acbb5a0bbe1692a8fd0e24ff2affee3d6cc57f41
Michael Cheung <michaelc1993@gmail.com>
2023-05-03 21:35:18 -0700

    Persist redux store to local storage with redux-persist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Notes from './components/Notes';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import Notes from './components/Notes';
+import { store, persistor } from './store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Notes />
+      <PersistGate loading={null} persistor={persistor}>
+        <Notes />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
